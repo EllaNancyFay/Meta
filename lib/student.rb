c6a9686c485d11e8a6cd88daa12646be51f796a4
EllaNancyFay <ellaschofield0@gmail.com>
2014-11-17 14:01:38 +0000

    Code

---
+++ lib/student.rb
@@ -0,0 +1,22 @@
+class Student
+
+  def initialize
+    @badges = []
+  end
+
+  def award(badge)
+    @badges << badge
+    self.class.send(:define_method, 'has_' + badge.to_s + '?') {
+      @badges.include? badge 
+    }
+  end
+
+  def method_missing(name)
+    return false
+  end
+
+  def create_method(name, block)
+  end
+
+end
+  
